7c6812f64c0499086d28816a44cbc857d62b2324
yeongbinim <yeim.dev@gmail.com>
2024-11-10 03:13:54 +0900

    241110: 오타 수정

--- 241106_파이어베이스-모듈화/src/request/dependency.js
+++ 241106_파이어베이스-모듈화/src/request/dependency.js
@@ -1,5 +1,5 @@
 import FirebaseRepository from "./FirebaseRepository.js";
-import Request from "./RequestRepository.js";
+import Request from "./Request.js";
 
 export function commentRepository() {
   return new Request("comments");
